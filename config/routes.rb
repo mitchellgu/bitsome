257dc97c21cb3b0e2bf9891ea92ac4b549e79793
Mitchell Gu <mx.mitchell.gu@gmail.com>
2014-11-01 01:53:52 -0400

    Separated homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'dashboard#show'
+   root 'static_pages#homepage'
+
+   get 'static_pages/homepage'
+   get 'dashboard/show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
